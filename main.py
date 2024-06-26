f66949fb4bd2d28baabe2f2f68644c6b73e209b9
Evan Giorgi <evan_giorgi@verizon.net>
2024-06-26 07:36:01 -0700

    Wrong Output

--- main.py
+++ main.py
@@ -5,15 +5,18 @@ def main():
     number1 = random.randint(0, 100)
     number2 = random.randint(0, 100)
     number3 = random.randint(0, 100)
-    """
-    ########################################
-    Code Your Program here
-    ########################################
-    """
-
-    ########################################
-    # Do not delete the return statement
-    ########################################
+    
+    if number1 < number2 and number1 < number3:
+        min_value = number1
+    else:
+        if number2 < number1 and number2 < number3:
+            min_value = number2
+        else:
+            min_value = number3
+    
+    print (number1, number2, number3)
+    print (min_value)
+    
     return number1, number2, number3, min_value
 
 
